cf5bb621851f4102953c0ee207ff5a84612343bb
VishwajeetTakle <vishu.22octafamous@gmail.com>
2026-05-09 13:43:02 +0530

    first testing
    first testing

---
+++ scripts/slider.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+  new Swiper(".fleet-swiper", {
+    slidesPerView: 1,
+    spaceBetween: 24,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+});
